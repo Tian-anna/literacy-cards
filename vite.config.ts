d28db01c9246b6ee9959251f15914eb617ed351a
Tian-anna <annatyf@sina.com>
2026-06-27 14:30:57 +0800

    add GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react";
 import path from "path";
 
 export default defineConfig({
+  base: "/literacy-cards/", // ← 添加这行
   plugins: [react()],
   resolve: {
     alias: {
