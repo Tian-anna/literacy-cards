eb857efee7b04d95fbee4aa903d812bee3fcc70b
Tian-anna <annatyf@sina.com>
2026-07-01 10:42:30 +0800

    修复

--- src/components/SceneManager.tsx
+++ src/components/SceneManager.tsx
@@ -127,7 +127,7 @@ const SceneManager: React.FC = () => {
           onChange={handleFileUpload}
           disabled={isUploading}
           className="absolute inset-0 opacity-0 cursor-pointer"
-          style={{ width: "100%", height: "100%" }}
+          style={{ width: "100%", height: "100%", fontSize: "16px" }}
         />
       </label>
 
