7a38b9c6199e70306bf1b035a382333f5c21ca41
Tian-anna <annatyf@sina.com>
2026-07-01 11:03:31 +0800

    修复

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,10 @@ import path from "path";
 export default defineConfig({
   plugins: [react()],
   base: "/literacy-cards/", // ← 添加这行
+  build: {
+    target: "safari11", // 兼容 Safari 11+
+    // 或 target: "es2015"  // 兼容 Safari 9+
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
